518451b0b6ed8dff6b0f0c02f8117b30b57a74a9
Rendi Ramadhan <ramadhanprogramrendi01@gmail.com>
2024-12-19 11:25:08 +0700

    gvhnvbn

--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,7 @@ use App\Http\Controllers\DataPemasukanController;
 use Illuminate\Support\Facades\Artisan;
 
 
-Route::domain('https://kurbanqu.online')->group(function () {
+Route::domain('')->group(function () {
 
     Route::get('/admin/info/edit', [TabelGambarController::class, 'edit'])->name('info.edit');
     Route::put('/admin/info/update', [TabelGambarController::class, 'update'])->name('info.update');
